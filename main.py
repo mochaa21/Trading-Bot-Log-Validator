dcf8647f99b3c3e3ccb64f326496984262953f92
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-07 11:58:19 +0700

    feat: add trade_execute

--- main.py
+++ main.py
@@ -24,7 +24,20 @@ raw_trade_log = """
 # TUGAS 2: Fungsi Utama
 # Eksekusi Type Annotations yang TEPAT (parsed json adalah dict).
 # Gunakan Sets untuk irisan dan selisih, lalu kembalikan hasilnya sebagai list.
+def trade_execute(data: str) -> str:
+    response: dict = json.loads(data)
+    indicator_a: str = response['data']['indikator_a_valid']
+    indicator_b: str = response['data']['indikator_b_valid']
+    false_breakout: list = response['data']['false_breakouts']
+    dataset_1: set = set(int(number) for number in indicator_a.strip().split(', '))
+    dataset_2: set = set(int(number) for number in indicator_b.strip().split(', '))
+    dataset_3: set = set(int(number) for number in false_breakout)
+    first_data_process: set = dataset_1.intersection(dataset_2)
+    end_data_process: set = first_data_process.difference(dataset_3)
+    new_data: list = sorted(end_data_process)
+    return f"Trade ID yang siap dieksekusi: {new_data}"
 
+print(trade_execute(raw_trade_log))
 
 
 # --- EKSEKUSI ---
